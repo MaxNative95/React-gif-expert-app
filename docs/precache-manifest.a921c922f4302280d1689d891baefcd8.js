840b3195f4b98227c57ce7f2de19c91c25ad0bc8
Max Garcia <maxgarcia@Maxs-MacBook-Pro.local>
2020-09-28 20:41:51 -0600

    Docs added

---
+++ docs/precache-manifest.a921c922f4302280d1689d891baefcd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741721d019d12486bb11c4b74ac2319b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "441eee0df61337b4b23c",
+    "url": "/static/css/main.5b0f2355.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441eee0df61337b4b23c",
+    "url": "/static/js/main.295b46ff.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
